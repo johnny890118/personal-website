1c4aae091e308df0c025d653d5701ed957876003
johnny890118 <johnny0929560027@gmail.com>
2024-03-07 22:17:40 +0800

    fix : skill進度條不正常顯示修改

--- components/skills.js
+++ components/skills.js
@@ -8,14 +8,14 @@ const Skills = () => {
           技能
         </p>
         <div className="flex w-[90%] flex-wrap items-center justify-center p-12 md:w-3/4">
-          <SkillsShow skillName={"JavaScript"} skillPercent={"80%"} />
-          <SkillsShow skillName={"React"} skillPercent={"60%"} />
-          <SkillsShow skillName={"Next.JS"} skillPercent={"50%"} />
-          <SkillsShow skillName={"Tailwind"} skillPercent={"70%"} />
-          <SkillsShow skillName={"SCSS"} skillPercent={"80%"} />
-          <SkillsShow skillName={"Python"} skillPercent={"70%"} />
-          <SkillsShow skillName={"C++"} skillPercent={"40%"} />
-          <SkillsShow skillName={"Git"} skillPercent={"70%"} />
+          <SkillsShow skillName={"JavaScript"} skillPercent={80} />
+          <SkillsShow skillName={"React"} skillPercent={60} />
+          <SkillsShow skillName={"Next.JS"} skillPercent={50} />
+          <SkillsShow skillName={"Tailwind"} skillPercent={70} />
+          <SkillsShow skillName={"SCSS"} skillPercent={80} />
+          <SkillsShow skillName={"Python"} skillPercent={70} />
+          <SkillsShow skillName={"C++"} skillPercent={40} />
+          <SkillsShow skillName={"Git"} skillPercent={70} />
         </div>
       </div>
     </div>
--- components/skillsShow.js
+++ components/skillsShow.js
@@ -4,10 +4,10 @@ const SkillsShow = ({ skillName, skillPercent }) => {
       <div className="m-4 flex h-52 w-52 flex-col items-center justify-center rounded-3xl shadow-2xl backdrop-blur-3xl">
         <div
           className="radial-progress text-violet-600"
-          style={{ "--value": 70 }}
+          style={{ "--value": skillPercent }}
           role="progressbar"
         >
-          {skillPercent}
+          {`${skillPercent}%`}
         </div>
         <div className="m-5">{skillName}</div>
       </div>
