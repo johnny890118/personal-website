ebeb4aa0a7d884d37f676edaa6640faf4879b56f
johnny890118 <johnny0929560027@gmail.com>
2024-03-07 14:37:40 +0800

    feat : 技能 UI

---
+++ components/skills.js
@@ -0,0 +1,21 @@
+const Skills = () => {
+  return (
+    <div className="h-[40rem] bg-[url('/bg2.jpg')] bg-cover bg-center md:h-dvh">
+      <div className="flex h-full w-full flex-col items-center justify-center backdrop-blur-sm">
+        <p className="mb-5 text-center">技能</p>
+        <div className="w-1/2 rounded-3xl bg-white p-12 shadow-2xl">
+          <p>JavaScript</p>
+          <p>React</p>
+          <p>Next.JS</p>
+          <p>Tailwind</p>
+          <p>SCSS</p>
+          <p>Git</p>
+          <p>Python</p>
+          <p>C++</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
