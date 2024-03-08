4442210bc5c9738a50c81d1085a2c476b197685f
johnny890118 <johnny0929560027@gmail.com>
2024-03-09 02:46:23 +0800

    refactor : 作品集補充

--- components/portfolio.js
+++ components/portfolio.js
@@ -18,23 +18,36 @@ const Portfolio = () => {
           portfolioURL={"https://mu-pho.vercel.app/"}
           portfolioImg={"/mupho.png"}
         />
+        <PortfolioShow
+          portfolioName={"To Do List"}
+          portfolioURL={
+            "https://johnny890118.github.io/toDoList.github.io/toDoList-demo/index.html"
+          }
+          portfolioImg={"/toDoList.png"}
+        />
         <p className="mb-5 border-b-4 border-cyan-400 pt-28 text-center text-2xl">
           UI組件
         </p>
         <div className="flex w-[90%] flex-wrap items-center justify-center md:w-[70%]">
           <UiComponentsShow
             portfolioName={"Alert"}
-            portfolioURL={"/"}
+            portfolioURL={
+              "https://johnny890118.github.io/alert-ui.github.io/alert-ui/index.html"
+            }
             portfolioImg={"/alert.png"}
           />
           <UiComponentsShow
             portfolioName={"Toast"}
-            portfolioURL={"/"}
+            portfolioURL={
+              "https://johnny890118.github.io/toast-ui.github.io//toast-ui/index.html"
+            }
             portfolioImg={"/toast.png"}
           />
           <UiComponentsShow
             portfolioName={"QA"}
-            portfolioURL={"/"}
+            portfolioURL={
+              "https://johnny890118.github.io/collapse-ui.github.io//QA/index.html"
+            }
             portfolioImg={"/QA.png"}
           />
         </div>
