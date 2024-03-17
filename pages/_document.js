352112d6797efc16649c98dd02b6a36961ef658d
johnny890118 <johnny0929560027@gmail.com>
2024-03-17 19:05:13 +0800

    refactor : bg rwd 調整

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[url('/bg2.jpg')] bg-cover bg-fixed bg-center">
+      <body className="bg-[url('/bg1.jpg')] bg-fixed bg-center xl:bg-[url('/bg2.jpg')] xl:bg-cover">
         <Main />
         <NextScript />
       </body>
