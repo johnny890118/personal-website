b390d4e4711038666a06faeed62e066a4d0368ef
johnny890118 <johnny0929560027@gmail.com>
2024-03-18 00:39:02 +0800

    refactor : mobile bg change

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[url('/bg1.jpg')] bg-fixed bg-center xl:bg-[url('/bg2.jpg')] xl:bg-cover">
+      <body className="bg-[url('/bg3.jpg')] bg-cover bg-fixed bg-center xl:bg-[url('/bg2.jpg')]">
         <Main />
         <NextScript />
       </body>
